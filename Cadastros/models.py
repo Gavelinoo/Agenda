8e500ae2183d2737194d794c4971ea1df749801d
Gaavelino <gleisonavelino@hotmail.com>
2025-12-05 00:35:14 -0300

    tabela dono

--- Cadastros/models.py
+++ Cadastros/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Donos(models.Model):
-    nome = models.CharField(max_length=100)
+class Dono(models.Model):
+    nome = models.CharField(max_length=50)
     idade = models.IntegerField()
     email = models.EmailField()
-    
+    endereco = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+
 
 
